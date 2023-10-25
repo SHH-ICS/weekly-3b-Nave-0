3602f72a40ec918988642f7bf96c6d3e91e3e274
Nave-0 <144241939+Nave-0@users.noreply.github.com>
2023-10-25 09:30:48 -0400

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,1 +1,14 @@
-
+import math
+large_price = (float(6.00))
+extralarge_price = (float(10.00))
+toppings = ['peppers','mushrooms','pinapple','chicken','olives']
+pizza_yn = input("would you perhaps like to order a pizza")
+for pizza in pizza_yn:
+    if pizza == "yes:":
+    print("next we'll get your toppings")
+     print(toppings) 
+topping1 = str(input(What would you like to be your first topping))
+topping2 = str(input(what would you like to be your second topping))
+else:
+    print("order is now canceled")
+    break
