8ff6c4ec18e175313eb3e63ae23207ab8a6191f0
Nave-0 <144241939+Nave-0@users.noreply.github.com>
2023-11-01 13:27:46 +0000

    higfsdg

--- assignment.py
+++ assignment.py
@@ -1,14 +1,17 @@
 import math
 large_price = (float(6.00))
 extralarge_price = (float(10.00))
-toppings = ['peppers','mushrooms','pinapple','chicken','olives']
+large_pricetopping1tax = (float(6.00 + 1.00 + 0.91))
+large_pricetopping2tax = (float(6.00 + 1.75 +1.01))
+large_pricetopping3tax = (float(6.00 + 2.50 + 1.11))
+large_pricetopping4tax = (float(6.00 + 3.35 + 1.22))
+toppings = ['1','2','3','4',]
 pizza_yn = input("would you perhaps like to order a pizza")
 for pizza in pizza_yn:
-    if pizza == "yes:":
+    if pizza == "yes or Yes or YES:":
     print("next we'll get your toppings")
      print(toppings) 
-topping1 = str(input(What would you like to be your first topping))
-topping2 = str(input(what would you like to be your second topping))
+topping1 = str(input(how many toppings would you like))
 else:
     print("order is now canceled")
     break
