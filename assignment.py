f51a508a5baa378951265a9a67b9a7fa211c74d9
Nave-0 <144241939+Nave-0@users.noreply.github.com>
2023-11-08 00:11:42 +0000

    what a monkey

--- assignment.py
+++ assignment.py
@@ -1,35 +1,53 @@
 import math
-large_price = (6.00)
-extralarge_price = (10.00)
-toppings = ['1','2','3','4',]
-topping1 = (1.00)
-topping2 = (1.75)
-topping3 = (2.50)
-topping4 = (3.35)
-Tax = (1.13)
-pizza_yn = input("would you perhaps like to order a pizza")
+size_price_map = {
+    'L' : 6.00,
+    'XL' : 10.00
+}
+
+topping_price_map = { 
+    '1' : 1.00,
+    '2' : 1.75,
+    '3' : 2.50,
+    '4' : 3.35
+}
+
+
+#toppings = ['1','2','3','4',]
+#topping1 = (1.00)
+#topping2 = (1.75)
+#topping3 = (2.50)
+#topping4 = (3.35)
+
+tax_rate = (0.13)
+
+pizza_yn = input("would you perhaps like to order a pizza (yes|no)")
+
+# The Size selected by the customer
+selected_size = 'None'
+
 while True: 
     if pizza_yn.lower() == 'yes':   
-        print("we will now take your order")
-    if pizza_yn.lower() == 'no':
-       print("order finished")
-       break 
-    var = input("what size pizza do you want, large or XL")
-    print("how many toppings would you like")
-    var = input(str("1,2,3 or 4"))
-    if "large" and 1:
-     print(large_price + topping1 * Tax)
-     if "large" and 2: 
-        print(large_price + topping2 * Tax)
-        if "large" and 3: 
-            print(large_price + topping3 * Tax)
-        if "large" and 4: 
-            print(large_price + topping4 * Tax)   
-            if "XL" and 1:
-                print(extralarge_price + topping1 * Tax)
-                if "XL" and 2:
-                    print(extralarge_price + topping2 * Tax)
-                    if "XL" and 3:
-                        print(extralarge_price + topping3 * Tax)
-                        if "XL" and 4:
-                            print(extralarge_price + topping4 * Tax)
+        selected_size = input("What size would you like (L|XL)")
+        
+        while True:
+            if selected_size in size_price_map:
+                print(selected_size + ' is valid\n')
+                #Put your logic to get the number of toppings here
+                #Remember:
+                #  Use the topping_price_map 
+                #  using 'break' to exit the loop will take you out into the previous while loop
+                #
+                #Once you have size and toppings you need to get the pricing.
+                #You do this by getting the value from each map using the key
+                #Calculate:
+                #  subtotal_amount (the price before tax)
+                #  tax_amount (just the tax)
+                #  total_amount (the total amount owed by the customer)
+            else:
+                print("Size entered is not valid")
+                break
+
+    else:
+        break
+ 
+
