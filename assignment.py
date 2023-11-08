89fde51b2f764e50b01cdafcfaea99410b087f42
Nave-0 <144241939+Nave-0@users.noreply.github.com>
2023-11-08 22:51:15 +0000

    helfdh

--- assignment.py
+++ assignment.py
@@ -10,44 +10,33 @@ topping_price_map = {
     '3' : 2.50,
     '4' : 3.35
 }
-
-
-#toppings = ['1','2','3','4',]
-#topping1 = (1.00)
-#topping2 = (1.75)
-#topping3 = (2.50)
-#topping4 = (3.35)
-
 tax_rate = (0.13)
-
 pizza_yn = input("would you perhaps like to order a pizza (yes|no)")
-
-# The Size selected by the customer
 selected_size = 'None'
-
+number_of_toppings = 'None'
 while True: 
     if pizza_yn.lower() == 'yes':   
         selected_size = input("What size would you like (L|XL)")
-        
         while True:
             if selected_size in size_price_map:
-                print(selected_size + ' is valid\n')
-                #Put your logic to get the number of toppings here
-                #Remember:
-                #  Use the topping_price_map 
-                #  using 'break' to exit the loop will take you out into the previous while loop
-                #
-                #Once you have size and toppings you need to get the pricing.
-                #You do this by getting the value from each map using the key
-                #Calculate:
-                #  subtotal_amount (the price before tax)
-                #  tax_amount (just the tax)
-                #  total_amount (the total amount owed by the customer)
+                print()
             else:
                 print("Size entered is not valid")
-                break
+                break 
+            while True: 
+                number_of_toppings = input("How many topping would you like (1,2,3,4)") 
+                if number_of_toppings in topping_price_map:
+                    price_of_toppings = topping_price_map[number_of_toppings]
+                    price_of_pizza = size_price_map[selected_size]
+                    subtotal = price_of_pizza + price_of_toppings
+                    print("Subtotal: " + str(subtotal))
+                    tax_amount = subtotal * tax_rate
+                    print("Tax Amount: " + str(tax_amount))
+                    total_cost = subtotal + tax_amount
+                    print("Total Cost: " + str(total_cost))
+                    break
+                else:
+                    print("amount of toppings entered is not valid")
+                    break
 
-    else:
-        break
- 
 
